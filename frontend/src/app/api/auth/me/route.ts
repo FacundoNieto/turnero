c9e4d370d94063f124a0f5fc50e2abc53d28ea1b
FacundoNieto <facundonieto24@gmail.com>
2026-05-25 14:58:08 -0300

    feat(/api/auth/me): verificación de variable_env

--- frontend/src/app/api/auth/me/route.ts
+++ frontend/src/app/api/auth/me/route.ts
@@ -17,7 +17,12 @@ const BACKEND = process.env.BACKEND_API_URL || process.env.NEXT_PUBLIC_API_URL |
 //   return new NextResponse(text, { status: res.status, headers: { 'Content-Type': 'application/json' } })
 // }
 export async function GET() {
-  console.log('==VALOR DE LA VARIABLE DE ENTORNO BACKEND ==:',BACKEND)
+  // console.log('==VALOR DE LA VARIABLE DE ENTORNO BACKEND ==:',BACKEND)
+  // TRUCO: En vez de loguear, devolvemos el valor directo a la pantalla para testear
+  return NextResponse.json({ 
+    mensaje: "Che, Vercel está leyendo esto:", 
+    valor_variable_env: BACKEND 
+  })
   try {
     const cookieStore = cookies()
     const token = cookieStore.get('access_token')?.value
